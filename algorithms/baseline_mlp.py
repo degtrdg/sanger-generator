5e6b087f2112812c704a4771f46b97c96cdb3c15
Daniel George <degtrdg@github.com>
2023-09-05 00:31:14 -0500

    Made baseline MLP model

---
+++ algorithms/baseline_mlp.py
@@ -0,0 +1,63 @@
+from torch.utils.data import Dataset
+import torch.nn as nn
+from sklearn.preprocessing import OneHotEncoder
+import os
+import pandas as pd
+import torch
+import numpy as np
+from Bio import SeqIO
+import torch.nn.functional as F
+
+class SangerSequencingDataset(Dataset):
+    def __init__(self, fasta_folder, csv_folder, seq_length):
+        csv_files = [file for file in os.listdir(csv_folder)]
+        csv_files = [file for file in csv_files if len(pd.read_csv(os.path.join(csv_folder, file)).values) > 0] # Don't include empty CSV files
+        fasta_files = [file.replace('.csv', '.fasta') for file in csv_files]
+
+        self.fasta_files = [os.path.join(fasta_folder, file) for file in fasta_files]
+        self.csv_files = [os.path.join(csv_folder, file) for file in csv_files]
+        self.seq_length = seq_length
+
+        self.encoder = OneHotEncoder(sparse_output=False, categories=[['A', 'C', 'G', 'T', 'N']])
+
+    def __len__(self):
+        return len(self.fasta_files)
+
+    def __getitem__(self, idx):
+        # Read FASTA file
+        fasta_sequence = str(list(SeqIO.parse(self.fasta_files[idx], "fasta"))[0].seq)
+
+        # Convert FASTA sequence to one-hot encoding
+        fasta_sequence = self.encoder.fit_transform(np.array(list(fasta_sequence)[:self.seq_length]).reshape(-1, 1))
+
+        # Read CSV file
+        csv_sequence = pd.read_csv(self.csv_files[idx]).values[:self.seq_length]
+
+        return torch.tensor(fasta_sequence, dtype=torch.float32), torch.tensor(csv_sequence, dtype=torch.float32)
+class SequenceToSangerMLP(nn.Module):
+    def __init__(self, input_size=1500, hidden_sizes=[1024, 512, 256], output_size=1200):
+        super(SequenceToSangerMLP, self).__init__()
+        
+        self.hidden_sizes = hidden_sizes
+        
+        self.fc1 = nn.Linear(input_size, self.hidden_sizes[0])
+        self.bn1 = nn.BatchNorm1d(self.hidden_sizes[0])
+        
+        self.fc2 = nn.Linear(self.hidden_sizes[0], self.hidden_sizes[1])
+        self.bn2 = nn.BatchNorm1d(self.hidden_sizes[1])
+        
+        self.fc3 = nn.Linear(self.hidden_sizes[1], self.hidden_sizes[2])
+        self.bn3 = nn.BatchNorm1d(self.hidden_sizes[2])
+        
+        self.fc4 = nn.Linear(self.hidden_sizes[2], output_size)
+    
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = F.relu(self.bn3(self.fc3(x)))
+        
+        x = self.fc4(x)
+        return x.view(64, 300, 4)
+
--- training.py
+++ training.py
@@ -4,18 +4,19 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import wandb
-from algorithms.cnn import SangerSequencingDataset, SequenceToSangerModel
+from algorithms.baseline_mlp import SangerSequencingDataset, SequenceToSangerMLP
 
 # WandB – Initialize a new run
 wandb.init(entity="degtrdg", project="sanger-generator")
 
 # WandB – Config is a variable that holds and saves hyperparameters and inputs
 config = wandb.config
-config.input_size = 5  # A, C, G, T, N
-config.hidden_size = 64
+config.input_size = 1500  # Updated input size. seq_length * 5. [A, C, G, T, N] 
+config.hidden_sizes = [1024, 512, 256]  # Updated hidden sizes
 config.seq_length = 300
 config.learning_rate = 1e-3
 config.epochs = 10
+config.output_size = 1200  # Added output size
 
 # Define data folders
 fasta_folder = 'data/fasta_files'
@@ -28,7 +29,8 @@ dataset = SangerSequencingDataset(fasta_folder, csv_folder, config.seq_length)
 dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
 
 # Initialize the model, loss, and optimizer
-model = SequenceToSangerModel(config.input_size, config.hidden_size, config.seq_length).to(device)
+model = SequenceToSangerMLP(config.input_size, config.hidden_sizes, config.output_size).to(device)  # Updated model parameters
+
 loss_function = nn.MSELoss()  # Mean Squared Error loss for regression
 optimizer = optim.Adam(model.parameters(), lr=config.learning_rate)
 
@@ -39,7 +41,8 @@ wandb.watch(model, log="all")
 # Training loop
 for epoch in range(config.epochs):
     for batch_fasta, batch_csv in dataloader:
-        batch_fasta = batch_fasta.transpose(2, 1).to(device)  # Adjusting dimensions to match Conv1d
+        # batch_fasta = batch_fasta.transpose(2, 1).to(device)  for CNN
+        batch_fasta = batch_fasta.to(device)  
         shape = batch_csv.shape
         # Reshape the tensor
         batch_csv_reshaped = batch_csv.view(-1, shape[-1])
